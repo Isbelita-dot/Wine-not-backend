4163704071ac63f29bcf373fd0a6a8bd1e97ef8c
jolo743 <jordigaar743@gmail.com>
2026-07-06 14:03:01 +0200

    feedback hecho, models hecho

---
+++ app/models/feedback.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+
+class feedback(Base):
+    __tablename__ = "feedback"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    custom_name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    rating: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False
+    )
+
+    comment: Mapped[str] = mapped_column(
+        String(500),
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    wine_id: Mapped[int] = mapped_column(
+        ForeignKey("wines.id")
+        nullable=False
+    )
+
+    wine: Mapped["Wine"] = relationship(
+        back_populates="feedbacks"
+    )
+

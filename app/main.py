7dd464cbb8b5780e4d5ae64f644610e13e55472c
jolo743 <jordigaar743@gmail.com>
2026-07-07 10:54:37 +0200

    app/main.py hecho

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.db.init_db import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI(
+    title="Wine Not",
+    description="Rest API para e-commerce Wine Not. ",
+    version="1.0.0",
+    lifespan=lifespan,
+)
+
+@app.get("/")
+def root():
+    return{
+        "message": "Bienvenido a la API de Wine Not!"
+    }
